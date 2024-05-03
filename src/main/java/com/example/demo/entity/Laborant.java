868de46a4edabe1f7bf0bae6f5d7275c5c6d5b22
sezerd <99814610+sezerdemir7@users.noreply.github.com>
2024-05-03 14:32:02 +0300

    create entity

--- src/main/java/com/example/demo/entity/Laborant.java
+++ src/main/java/com/example/demo/entity/Laborant.java
@@ -1,13 +1,24 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Entity
 @Data
 public class Laborant extends BaseEntity{
+    @Id
+    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private String ad;
     private String soyad;
     private String kimlikNumarasi;
+
+
+
+
+
 }
--- src/main/java/com/example/demo/entity/Patient.java
+++ src/main/java/com/example/demo/entity/Patient.java
@@ -1,13 +1,22 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Entity
 @Data
 public class Patient extends BaseEntity{
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String ad;
     private String soyad;
     private String kimlikNumarasi;
+
+
+
 }
