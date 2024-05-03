fb044e44ec6f75daae254098b2e0a69b5353106e
Eray Akgül <90953626+Erayakg@users.noreply.github.com>
2024-05-03 13:58:07 +0300

    first commit

---
+++ src/main/java/com/example/demo/LaboratoryAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratoryAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratoryAutomationApplication.class, args);
+	}
+
+}
