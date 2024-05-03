e1411ec19b1173295e94f8a20e07aea459861f17
sezerd <99814610+sezerdemir7@users.noreply.github.com>
2024-05-03 17:38:05 +0300

    reportcontroller updated

--- src/main/java/com/example/demo/services/ReportService.java
+++ src/main/java/com/example/demo/services/ReportService.java
@@ -1,7 +1,6 @@
 package com.example.demo.services;
 
 import com.example.demo.dto.ReportReqDTO;
-import com.example.demo.dto.ReportResDTO;
 import com.example.demo.dto.ReportResIdDTO;
 import com.example.demo.entity.Report;
 import org.springframework.stereotype.Service;
@@ -20,8 +19,8 @@ public interface ReportService {
     Report getbyid(Long id);
 
     List<Report> reportList();
-    List<ReportResDTO> getReportListByLaboratoryWorkerId(Long laboratoryWorkerId);
+    List<ReportResIdDTO> getReportListByLaboratoryWorkerId(Long laboratoryWorkerId);
 
 
-    List<ReportResDTO> getReportListByPatientId(Long patientId);
+    List<ReportResIdDTO> getReportListByPatientId(Long patientId);
 }
